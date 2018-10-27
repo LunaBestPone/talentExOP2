5813a95e625bc624e7b61daccbb0b360a07e5c1b
LunaBestPone <saltyprinx@gmail.com>
2018-10-26 21:42:07 -0500

    Added rest_auth.urls in mapping

--- backend/dj/dj/urls.py
+++ backend/dj/dj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
+    path('rest_auth/', include('rest_auth.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('main.api.urls')),
 ]
