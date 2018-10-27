459657f351dd776f385237dc56eb1559eebfa691
LunaBestPone <saltyprinx@gmail.com>
2018-10-26 21:58:05 -0500

    fixed routing for registration

--- backend/dj/dj/urls.py
+++ backend/dj/dj/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest_auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls'))
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('main.api.urls')),
 ]
