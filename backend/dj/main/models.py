9c4891c3ef0c31c4bcbf5c7263341a72cfaf3e61
LunaBestPone <saltyprinx@gmail.com>
2018-10-28 00:58:33 -0500

    Revert "now MyUser extends User"
    This reverts commit 19d4fb771127b61f69ee3e3a18530b68a1c56168.

--- backend/dj/main/models.py
+++ backend/dj/main/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class MyUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+# temporary user entity set
+class User(models.Model):
+    # set user_name as primary key
+    user_name = models.CharField(max_length=200, primary_key = True)
+    password = models.CharField(max_length=200, default='')
+    email = models.CharField(max_length=200, null=True)
     age = models.IntegerField(null=True)
-    user_rating = models.FloatField(default=5.0)
+    user_rating = models.FloatField(default=0.0)
     learning_credit = models.IntegerField(default=10)
 
     def __str__(self):
-        return self.user.username
+        return self.user_name
