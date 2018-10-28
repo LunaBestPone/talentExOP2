97b5c85b8db64aec83451f7481a37af55ebd9a15
LunaBestPone <saltyprinx@gmail.com>
2018-10-28 13:52:45 -0500

    created WorkshopListView.js, it contains instructions that might be helpful

---
+++ frontend/gui/src/containers/WorkshopListView.js
@@ -0,0 +1,4 @@
+// TODO: grab a list component from antd, for general design guideline for the project, please refer to UserDetailView.js
+// for axios calls, the path is 'http://127.0.0.1:8000/api/workshop' for list, 'http://127.0.0.1:8000/api/workshop/detail/' + ws_id for detail, 'http://127.0.0.1:8000/api/workshop/create' for creating workshop.
+// in this file, implment list view exclusively.
+// for Detail, please create a seperate component, aka WorkshopDetailView.js in similar manner.
