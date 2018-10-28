6d9ed7418fc104ed18c6f1abdee830319f1e5df4
LunaBestPone <saltyprinx@gmail.com>
2018-10-28 13:37:26 -0500

    added serializers for workshop and enrollment

--- backend/dj/main/api/serializer.py
+++ backend/dj/main/api/serializer.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 
-from main.models import User
+from main.models import User, Workshop, Enrollment
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class WorkshopSerializer(serializers.ModelSerializer):
+        model = Workshop
+        fields = '__all__'
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+        model = Enrollment
         fields = '__all__'
