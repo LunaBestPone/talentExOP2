3260134526b9b8593f852d4534dd603dbef3054e
LunaBestPone <saltyprinx@gmail.com>
2018-10-28 13:38:35 -0500

    added generic views for workshop and enrollment

--- backend/dj/main/api/views.py
+++ backend/dj/main/api/views.py
@@ -1,14 +1,33 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 
-from main.models import User
-from .serializer import UserSerializer
+from main.models import User, Workshop, Enrollment
+from .serializer import UserSerializer, WorkshopSerializer, EnrollmentSerializer
 
 class UserListView(ListAPIView):
-        queryset = User.objects.all()
-        serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class UserDetailView(RetrieveAPIView):
-        queryset = User.objects.all()
-        serializer_class = UserSerializer
-        
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class WorkshopListView(ListAPIView):
+    queryset = Workshop.objects.all()
+    serializer_class = WorkshopSerializer
+
+class WorkshopDetailView(RetrieveAPIView):
+    queryset = Workshop.objects.all()
+    serializer_class = WorkshopSerializer
+
+class WorkshopCreateView(CreateAPIView):
+    queryset = Workshop.objects.all()
+    serializer_class = WorkshopSerializer
+
+class EnrollmentListView(ListAPIView):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+
+class EnrollmentDetailView(RetrieveAPIView):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
