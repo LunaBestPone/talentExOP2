75067e1f089fcfa3606cf5b93544fbbba846ecf7
LunaBestPone <saltyprinx@gmail.com>
2018-10-28 01:11:13 -0500

    Registered UserAdmin

--- backend/dj/main/admin.py
+++ backend/dj/main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 # admin user name: default
 # password: 506talentexchange
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
