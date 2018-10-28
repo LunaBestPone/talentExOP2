d14ecc1a7bc976b2a512ab421b3d8314e2984d56
LunaBestPone <saltyprinx@gmail.com>
2018-10-28 13:51:28 -0500

    added skeleton for workshoplist routing, uncomment them to make it work

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from  'react-router-dom';
 
+// TODO: import WorkshopList from './containers/WorkshopListView';
 import UserDetail from './containers/UserDetailView';
 import Register from './containers/Register';
 import Signup from './containers/Signup';
@@ -8,6 +9,7 @@ import Login from './containers/Login';
 
 const BaseRouter = () => (
   <div>
+    // TODO: <Route exact path='/' component={WorkshopList} />
     <Route exact path='/user/:user_name/' component={UserDetail} />
     <Route exact path='/login/' component={Login} />
     <Route exact path='/register/' component={Register} />
