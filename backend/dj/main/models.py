15fb032acfe56aaa011a6d6e40e78fa305a2be27
LunaBestPone <saltyprinx@gmail.com>
2018-10-28 12:35:52 -0500

    Added Workshop and Enrollment models

--- backend/dj/main/models.py
+++ backend/dj/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import django.utils import timezone
 
 class User(AbstractUser):
     age = models.IntegerField(null=True)
@@ -8,3 +9,19 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Workshop(models.Model):
+    ws_id = models.AutoField(default = 1, primary_key = True)
+    ws_name = models.CharField(max_length = 200)
+    host_user = models.ForeignKey(User, on_delete = models.PROTECT)
+    min_cap = models.IntegerField(null = True)
+    max_cap = models.IntegerField(null = True)
+    is_active = models.BooleanField(default = True)
+    description = models.TextField(null = True)
+    start_date_time = models.DateTimeField(default = timezone.now)
+    end_date_time = models.DateTimeField(default = timezone.now)
+
+class Enrollment(models.Model)
+    ws_id = models.ForeignKey(Workshop, on_delete = models.DELETE)
+    enrolled_user = models.ForeignKey(User, on_delete = models.DELETE)
+    enroll_date_time = models.DateTimeField(default = timezone.now)
