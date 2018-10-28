c9cae1c19777b11a5ffed38bd3ce028ca33d2057
LunaBestPone <saltyprinx@gmail.com>
2018-10-28 13:38:07 -0500

    added routing for workshop and enrollment api

--- backend/dj/main/api/urls.py
+++ backend/dj/main/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import UserListView, UserDetailView
+from .views import UserListView, UserDetailView, WorkshopListView, WorkshopDetailView, WorkshopCreateView, EnrollmentListView, EnrollmentDetailView
 
 urlpatterns = [
-    path('', UserListView.as_view()),
-    path('<pk>', UserDetailView.as_view()),
+    path('user/', UserListView.as_view()),
+    path('user/<pk>', UserDetailView.as_view()),
+    path('workshop/', WorkshopListView.as_view()),
+    path('workshop/detail/<pk>', WorkshopDetailView.as_view()),
+    path('workshop/create', WorkshopCreateView.as_view()),
+    path('enrollment/', EnrollmentListView.as_view()),
+    path('enrollment/<pk>', EnrollmentDetailView.as_view()),
 ]
