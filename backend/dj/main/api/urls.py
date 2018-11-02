d7ef2e57bb0bd815623cf1962509380dbf0bc716
LunaBestPone <saltyprinx@gmail.com>
2018-11-02 11:49:38 -0500

    some modification to routing

--- backend/dj/main/api/urls.py
+++ backend/dj/main/api/urls.py
@@ -4,11 +4,11 @@ from .views import UserListView, UserDetailView, WorkshopListView, WorkshopDetai
 
 urlpatterns = [
     path('user/', UserListView.as_view()),
-    path('user/<pk>', UserDetailView.as_view()),
+    path('user/<pk>/', UserDetailView.as_view()),
     path('workshop/', WorkshopListView.as_view()),
-    path('workshop/detail/<pk>', WorkshopDetailView.as_view()),
-    path('workshop/create', WorkshopCreateView.as_view()),
+    path('workshop/detail/<pk>/', WorkshopDetailView.as_view()),
+    path('workshop/create/', WorkshopCreateView.as_view()),
     path('enrollment/', EnrollmentListView.as_view()),
-    path('enrollment/detail/<pk>', EnrollmentDetailView.as_view()),
-    path('enrollment/create',EnrollmentCreateView.as_view()),
+    path('enrollment/detail/<pk>/', EnrollmentDetailView.as_view()),
+    path('enrollment/create/',EnrollmentCreateView.as_view()),
 ]
