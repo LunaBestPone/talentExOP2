58ab84a90d453d0b8a8fdac475c6043d5717771f
LunaBestPone <saltyprinx@gmail.com>
2018-11-01 23:45:22 -0500

    added create enrollment url

--- backend/dj/main/api/urls.py
+++ backend/dj/main/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import UserListView, UserDetailView, WorkshopListView, WorkshopDetailView, WorkshopCreateView, EnrollmentListView, EnrollmentDetailView
+from .views import UserListView, UserDetailView, WorkshopListView, WorkshopDetailView, WorkshopCreateView, EnrollmentListView, EnrollmentDetailView, EnrollmentCreateView
 
 urlpatterns = [
     path('user/', UserListView.as_view()),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('workshop/create', WorkshopCreateView.as_view()),
     path('enrollment/', EnrollmentListView.as_view()),
     path('enrollment/<pk>', EnrollmentDetailView.as_view()),
+    path('enrollment/create',EnrollmentCreateView.as_view()),
 ]
