0b3679a24e9c3373f838276f6ee58ab6c719f5e9
haoysh <syoun6@wisc.edu>
2018-11-14 20:53:50 -0600

    Placeholders for sort added

--- frontend/gui/src/containers/Sort.js
+++ frontend/gui/src/containers/Sort.js
@@ -15,22 +15,55 @@ const Option = Select.Option;
 //   console.log('focus');
 // }
 
+//Probably want props of subject lists, location lists, start time lists, end time lists.
+//Want any in each select comp
 const Sort = (props) => {
     return(
-        <Select
-            showSearch
-            style={{ width: 300 }}
-            placeholder="Sort by subject"
-            optionFilterProp="children"
-            // onChange={handleChange}
-            // onFocus={handleFocus}
-            // onBlur={handleBlur}
-            filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-        >
-            <Option value= "Calculus I">Calculus I</Option>
-            <Option value= "Linear Algebra">Linear Algebra</Option>
-            <Option value= "Language">Language</Option>
-        </Select>
+        <div>
+            <Select
+                showSearch
+                style={{ width: 300 }}
+                placeholder="Sort by subject"
+                optionFilterProp="children"
+                // onChange={handleChange}
+                // onFocus={handleFocus}
+                // onBlur={handleBlur}
+                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+                <Option value= "Any">Any</Option>
+                <Option value= "Calculus I">Calculus I</Option>
+                <Option value= "Linear Algebra">Linear Algebra</Option>
+                <Option value= "Language">Language</Option>
+            </Select>
+            <Select
+                showSearch
+                style={{ width: 300 }}
+                placeholder="Sort by location"
+                optionFilterProp="children"
+                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+                <Option value= "Any">Any</Option>
+            </Select>
+            <Select
+                showSearch
+                style={{ width: 300 }}
+                placeholder="Sort by start time"
+                optionFilterProp="children"
+                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+                <Option value= "Any">Any</Option>
+            </Select>
+            <Select
+                showSearch
+                style={{ width: 300 }}
+                placeholder="Sort by end time"
+                optionFilterProp="children"
+                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+                <Option value= "Any">Any</Option>
+            </Select>
+
+        </div>
     );
 }
 export default Sort;
