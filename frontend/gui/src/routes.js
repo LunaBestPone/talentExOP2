a39905da06718007c758fd5d4377ac716ee94492
XIAOCHAO YAN <leon_whcb@yahoo.com>
2018-11-14 21:33:12 -0600

    Added a new Tab calledd "WorkShopMap" Could take location inforamtion in database and display. Will improve apperaence if time allowed.

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -10,6 +10,7 @@ import Login from './containers/Login';
 import WorkshopDetail from './containers/WorkshopDetail';
 import WorkshopList from './containers/WorkshopListView';
 import Home from './containers/Home';
+import WorkshopMap from './containers/WorkShopMap';
 
 const BaseRouter = () => (
   <div>
@@ -20,6 +21,7 @@ const BaseRouter = () => (
     <Route exact path='/' component={Home} />
     <Route exact path='/user/:user_pk/' component={UserDetail} />
     <Route exact path='/workshop/' component={WorkshopList} />
+    <Route exact path='/workshopmap/' component={WorkshopMap} />
     <Route exact path='/workshop/detail/:ws_id/' component={WorkshopDetail} />
     <Route exact path='/login/' component={Login} />
     <Route exact path='/createws/' component={Create} />
