81edeb88d1655629ea03e90bd05d280a35ee073f
wlaine <wclaine@gmail.com>
2018-11-15 19:20:57 -0600

    skeleton for edit workshop

---
+++ frontend/gui/src/components/TextInput.js
@@ -0,0 +1,30 @@
+import React, {PropTypes} from 'react';
+
+const TextInput = ({name, label, onChange, placeholder, value}) => {  
+  return (
+    <div className="form-group">
+      <div className="field">
+        {label}{' '}
+        <input
+          style = {{width: '25%'}}
+          type="text"
+          name={name}
+          className="form-control"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}/>
+      </div>
+    </div>
+  );
+};
+
+{/*
+TextInput.propTypes = {  
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string
+};
+*/}
+
+export default TextInput;  
