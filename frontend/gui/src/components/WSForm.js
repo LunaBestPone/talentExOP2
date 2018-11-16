79ad79431299b8b9fc355343f6ee6b13caa1042c
wlaine <wclaine@gmail.com>
2018-11-16 00:59:20 -0600

    edit workshop enhancement

--- frontend/gui/src/components/WSForm.js
+++ frontend/gui/src/components/WSForm.js
@@ -1,73 +1,80 @@
-import React, {PropTypes} from 'react';  
+import React, { PropTypes } from 'react';
 import TextInput from './TextInput';
 
-class WSForm extends React.Component {  
+class WSForm extends React.Component {
   render() {
     return (
       <div>
         <form>
           <span>
             <b>workshop id:</b>{this.props.workshop.ws_id}
-          </span> 
+          </span>
 
           <TextInput
             name="ws_name"
             label="name"
             value={this.props.workshop.ws_name}
-            onChange={this.props.onChange}/>
+            onChange={this.props.onChange} />
 
-            <span>
-              <b>host user:</b> {this.props.workshop.host_user}
-            </span> 
+          <span>
+            <b>host user:</b> {this.props.workshop.host_username}
+          </span>
 
-            <TextInput
+          <TextInput
             name="category"
             label="category"
             value={this.props.workshop.category}
-            onChange={this.props.onChange}/>
+            onChange={this.props.onChange} />
 
-            <TextInput
+          <TextInput
             label="min attendees"
             name="min_cap"
             value={this.props.workshop.min_cap}
-            onChange={this.props.onChange}/>
+            onChange={this.props.onChange} />
 
-            <TextInput
+          <TextInput
             label="max attendes"
             name="max_cap"
             value={this.props.workshop.max_cap}
-            onChange={this.props.onChange}/>
+            onChange={this.props.onChange} />
 
-            <span>
-              <b>is active: </b> {this.props.workshop.is_active}
-            </span> 
-           
-            <TextInput
+          <span>
+            <b>is active: </b> {this.props.workshop.is_active}
+          </span>
+
+          <TextInput
             name="description"
             label="description"
             value={this.props.workshop.description}
-            onChange={this.props.onChange}/>
+            onChange={this.props.onChange} />
 
-            <TextInput
+          <TextInput
             name="start_date_time"
             label="start time"
             value={this.props.workshop.start_date_time}
-            onChange={this.props.onChange}/>
+            onChange={this.props.onChange} />
 
-            <TextInput
+          <TextInput
             name="end_date_time"
             label="end time"
             value={this.props.workshop.end_date_time}
-            onChange={this.props.onChange}/>
+            onChange={this.props.onChange} />
           <br></br>
           <input
             type="submit"
             disabled={this.props.saving}
             className="btn btn-primary"
-            onClick={this.props.onSave}/>
+            onClick={this.props.onSave} />
+          {' '}
+          <input
+            type="submit"
+            value="Cancel"
+            disabled={this.props.saving}
+            className="btn btn-primary"
+            onClick={this.props.cancelEdit}/>
         </form>
       </div>
-  );
+    );
   }
 }
 
