6269e9c669a2ecf74362ba8798c67c74df44e1b2
haoysh <syoun6@wisc.edu>
2018-11-16 23:15:13 -0600

    tests and necessary modules to test

---
+++ frontend/gui/src/containers/Create.test.js
@@ -0,0 +1,21 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json'; //added this line
+import Create from './Create';
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Testing Creating component', () => {
+  
+    it('renders as expected', () => {
+      const setRouteLeaveHook = jest.fn();
+      const wrapper = shallow(
+        <Create.WrappedComponent params={{router: 
+          setRouteLeaveHook}}/>
+      );
+     expect(wrapper.state().st).toEqual("");     
+   });
+});
