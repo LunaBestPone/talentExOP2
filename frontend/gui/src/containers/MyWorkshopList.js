6895d03ad3608b525d3a6b48cdbd1f11732c2b17
Zhaoqi Li <zli484@wisc.edu>
2018-11-29 21:18:03 -0600

    Two tabs for seeing both workshop hosting and workshop participating

--- frontend/gui/src/containers/MyWorkshopList.js
+++ frontend/gui/src/containers/MyWorkshopList.js
@@ -1,15 +1,22 @@
-// TODO: grab a list component from antd, for general design guideline for the project, please refer to UserDetailView.js
-// for axios calls, the path is 'http://127.0.0.1:8000/api/workshop' for list, 'http://127.0.0.1:8000/api/workshop/detail/' + ws_id for detail, 'http://127.0.0.1:8000/api/workshop/create' for creating workshop.
-// in this file, implment list view exclusively.
-// for Detail, please create a seperate component, aka WorkshopDetailView.js in similar manner.
+
 
 import React from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
-import { List, Icon, Button, Row, Col, Collapse } from 'antd';
+import { Card, List, Icon, Button, Row, Col, Collapse } from 'antd';
 
 import Workshop from '../components/Workshop';
+
+
+const tabListNoTitle = [{
+  key: 'hosting',
+  tab: 'Hosting Workshops',
+}, {
+  key: 'participating',
+  tab: 'Participating Workshops',
+}];
+
 
 const Panel = Collapse.Panel;
 const stylebutton = {
@@ -17,30 +24,36 @@ const stylebutton = {
 }
 
 class MyWorkshopList extends React.Component {
+
+
   constructor(props, context) {
     super(props, context);
     this.state = {
-      workshops: [],
-      isRegistered: false,
+      hostWorkshops: [],
+      enrolledWorkshops: [],
+      //isRegistered: false,
+      noTitleKey:'hosting',
       user: {}
     };
   }
 
   componentDidMount() {
+
     if (this.props.isAuthenticated) {
       //workshops user is host of
       axios.get('http://127.0.0.1:8000/api/workshop/?host_user' + "=" + this.props.user)
         .then(res => {
           this.setState({
-            workshops: res.data,
+            hostWorkshops: res.data,
           });
 
         })
+
       //workshops user is enrolled in
-      axios.get('http://127.0.0.1:8000/api/user/?enrollment' + "=" + this.props.workshops)
+      axios.get('http://127.0.0.1:8000/api/user/?enrollment' + "=" + this.props.user)
         .then(res => {
           this.setState({
-            workshop: res.data,
+            enrolledWorkshops: res.data,
           })
         })
     }
@@ -51,21 +64,56 @@ class MyWorkshopList extends React.Component {
     }
   }
 
+    onTabChange = (key, type) => {
+    console.log(key, type);
+    this.setState({ [type]: key });
+  }
 
   render() {
+
     const user_id = this.props.user;
     console.log("render");
 
-    return (
-      <div>
-        <h1>My Workshops</h1>
-        <p><NavLink to="/workshopmap/">Click for Map View</NavLink></p>
-        <Row gutter={16}>
+    const contentListNoTitle = {
+      hosting: <p>
+
+          <Row gutter={16}>
+            <Col span={7} offset={1}>
+              <List
+                grid={{ gutter: 16, column: 1 }}
+                dataSource={this.state.hostWorkshops}
+                renderItem={item => (
+                  //if(isRegistered ) {
+                  <List.Item>
+                    <Workshop
+                      ws_id={item.ws_id}
+                      ws_name={item.ws_name}
+                      host_user={item.host_user}
+                      category={item.category}
+                      min_cap={item.min_cap}
+                      max_cap={item.max_cap}
+                      is_active={item.is_active}
+                      description={item.description}
+                      start_time_display={item.start_time_display}
+                      end_time_display={item.end_time_display}
+                      is_detailed={false} />
+                  </List.Item>
+                  //}
+                )}
+              />
+            </Col>
+          </Row>
+      
+            </p>,
+
+      participating: <p>
+          <Row gutter={16}>
           <Col span={7} offset={1}>
             <List
               grid={{ gutter: 16, column: 1 }}
-              dataSource={this.state.workshops}
+              dataSource={this.state.enrolledWorkshops}
               renderItem={item => (
+                //if(isRegistered ) {
                 <List.Item>
                   <Workshop
                     ws_id={item.ws_id}
@@ -80,10 +128,32 @@ class MyWorkshopList extends React.Component {
                     end_time_display={item.end_time_display}
                     is_detailed={false} />
                 </List.Item>
+                //}
               )}
             />
           </Col>
         </Row>
+
+      </p>,
+      //project: <p>project content</p>,
+    };
+
+    
+
+    return (
+
+      <div>
+
+        <h1>My Workshops</h1>
+        <Card
+          style={{ width: '100%' }}
+          tabList={tabListNoTitle}
+          activeTabKey={this.state.noTitleKey}
+          onTabChange={(key) => { this.onTabChange(key, 'noTitleKey'); }}
+        >
+          {contentListNoTitle[this.state.noTitleKey]}
+
+        </Card>
       </div>
     )
 
