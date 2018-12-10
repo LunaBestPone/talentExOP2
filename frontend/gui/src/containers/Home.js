35f4603b41b0433553874aba0bb41c4954fc593d
Zhaoqi Li <zli484@wisc.edu>
2018-12-09 21:26:33 -0600

    updated homepage-second try

--- frontend/gui/src/containers/Home.js
+++ frontend/gui/src/containers/Home.js
@@ -1,24 +1,115 @@
 import React from 'react';
 import { Card } from 'antd';
+import {Layout} from 'antd';
+import { Carousel, Grid,Jumbotron, Button, Row, Col} from 'react-bootstrap';
+import './stylesheet.css';
+
+
+
+
+
+const {
+  Header, Footer, Sider, Content,
+} = Layout;
 
 class Home extends React.Component {
     render() {
-        return(
-            <Card title="Welcome to Talent Exchange and Meet Up Project!" bordered={false} style={{ width: '100%' }}>
-                <p>This is a CS 506 Team Project by <br></br>
-                Yong Jae Cho, Charles Grosz, William Laine, Jamie Lee, Zihan Wang, Xiaochao Yan, Sung Ho Youn, Hao Yuan, Zhaoqi Li</p>
-                <p>The github could be found <a href="https://github.com/LunaBestPone/talentExOP2">HERE</a></p>
-                <p>The purpose of this application is to build a community for
-                talent sharing. You can sign up available workshops or
-                create your workshops. For each workshop you registered
-                to particiate, 1 learning credit will be deducted. Similarly,
-                If you host a workshop, the number of credits you earn will be
-                based on the number of participants. If you find bugs, whoa,
-                we encourage you to open issues on our repository :)
-                </p>
-            </Card>
+
+    return(
+
+    <Grid>
+        <Row class='carousel-top'>
+             <Carousel>
+    
+              <Carousel.Item>
+                <img width={1200} height={300} alt="900x600" src={require('../images/beginning.png')} />
+                <Carousel.Caption>
+                  <h3></h3>
+                  <p></p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>;
+        </Row>
+
+        <hr class="line-break" />   
+
+        <Row>
+            <Col md={5}>
+              <img class='img' src={require('../images/workshop-1.jpg')}/>    
+            </Col>
+            <Col md={7}>
+                  <h1 className='title'>Talent</h1>
+                  <p className='subtitle'>
+                  Register a workshop to learn something that you do not know before, it has never been easy like this.
+                  </p>
+                  <p class='button-more'>
+                    <Button bsStyle="primary">Browse workshop</Button>
+                  </p>
+            </Col>
+        </Row>
+
+        <hr class="line-break" />
+
+         <Row>
+             <Col md={7}>
+                  <h1 className='title'>Skill</h1>
+                  <p className='subtitle'>
+                  Register a workshop to learn something that you do not know before, it has never been easy like this.
+                  </p>
+                  <p class='button-more'>
+                    <Button bsStyle="primary">Browse workshop</Button>
+                  </p>
+            </Col>
+            <Col md={5}>
+              <img class='img' src={require('../images/workshop-1.jpg')}/>   
+            </Col>
+
+
+        </Row>
+
+        <hr class="line-break" />
+
+
+        <Row>
+            <Col md={5}>
+              <img class='img' src={require('../images/workshop-1.jpg')}/> 
+            </Col>
+            <Col md={7}>
+                  <h1 className='title'>Study</h1>
+                  <p className='subtitle'>
+                  Register a workshop to learn something that you do not know before, it has never been easy like this.
+                  </p>
+                  <p class='button-more'>
+                    <Button bsStyle="primary">Browse workshop</Button>
+                  </p>
+            </Col>
+        </Row>
+
+        <hr class="line-break" />   
+
+         <Row>
+                 <Col md={7}>
+                      <h1 className='title'>Community</h1>
+                      <p className='subtitle'>
+                      Host a study group on Talent & Exchange and help each other to learn knolwedge from different subjects or prepare for final together.
+                      </p>
+                      <p class='button-more'>
+                        <Button bsStyle="primary">Browse workshop</Button>
+                      </p>
+
+                </Col>
+                <Col md={5}>
+                  <img class='img' src={require('../images/workshop-1.jpg')}/>   
+                </Col>
+
+
+        </Row>
+
+    </Grid>
+
         )
-    }
+  }
+    
 }
 
 export default Home;
