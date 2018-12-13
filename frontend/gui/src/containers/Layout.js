9da9e977eec1fbebe687ed26887a699240210365
haoysh <syoun6@wisc.edu>
2018-12-13 13:04:04 -0600

    Profile tab not visible when not logged in

--- frontend/gui/src/containers/Layout.js
+++ frontend/gui/src/containers/Layout.js
@@ -28,10 +28,12 @@ class CustomLayout extends React.Component {
             defaultSelectedKeys={['0']}
             style={{ lineHeight: '64px', float: 'left' }}
           >
-            <Menu.Item className="item1" key="1" onClick = {() => this.updatePage("Home")}><Link to="/">Talent Exchange</Link></Menu.Item>
+            <Menu.Item className="item1" key="1" onClick = {() => this.updatePage("Home")}>
+              <Link to="/">Talent Exchange</Link></Menu.Item>
+              
             {
             this.props.isAuthenticated ?
-            <Menu.Item className="item21" key="2" onClick = {this.props.logout} >Logout</Menu.Item>
+            <Menu.Item className="item21" key="2" onClick = {this.props.logout} ><Link to="/">Logout</Link></Menu.Item>
             :
             <Menu.Item className="item22" key="2" onClick = {() => this.updatePage("Log In")}><Link to="/login/">Log in</Link></Menu.Item>
             }
@@ -39,7 +41,7 @@ class CustomLayout extends React.Component {
             <Menu.Item className="item4" key="4" onClick={() => this.updatePage("Workshop List")}><Link to="/workshop/">Workshop</Link></Menu.Item>
             {this.props.isAuthenticated ? <Menu.Item className="item5" key="5" onClick={() => this.updatePage("My Workshops")}><Link to="/MyWorkshopList/">My Workshops</Link></Menu.Item> : null}
             <Menu.Item className="item6" key="6" onClick = {() => this.updatePage("About Us")}><Link to="/about/">About us</Link></Menu.Item>
-            <Menu.Item className="item7" key="7" onClick = {() => this.updatePage("profile")}><Link to="/profile/">Profile</Link></Menu.Item>
+            {this.props.isAuthenticated ? <Menu.Item className="item7" key="7" onClick = {() => this.updatePage("profile")}><Link to="/profile/">Profile</Link></Menu.Item> : null}
           </Menu>
         </Header>
 
